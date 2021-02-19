60920ba027d5af5babe8a6f71477ffeec6fea554
Dean Jennings <puffin.develop@gmail.com>
2021-02-19 13:48:42 +0000

    Add basic tag loading functionality

--- Easy-ID3.py
+++ Easy-ID3.py
@@ -1,1 +1,13 @@
 import eyed3
+
+
+filepath = input("Type a filepath: ")
+
+try:
+    file = eyed3.load(filepath)
+except FileNotFoundError:
+    print("That file does not exist.")
+    exit()
+
+print("Artist: " + file.tag.artist)
+print("Album: " + file.tag.album)
