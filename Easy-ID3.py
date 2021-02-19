f34eeb76f4c6b7da52c63bbbe039b83a5cda9da2
Dean Jennings <puffin.develop@gmail.com>
2021-02-19 13:54:06 +0000

    Add basic tag editing functionality

--- Easy-ID3.py
+++ Easy-ID3.py
@@ -5,9 +5,15 @@ filepath = input("Type a filepath: ")
 
 try:
     file = eyed3.load(filepath)
-except FileNotFoundError:
+except OSError:
     print("That file does not exist.")
     exit()
 
 print("Artist: " + file.tag.artist)
 print("Album: " + file.tag.album)
+print("\n")
+
+file.tag.artist = input("What do you want to change the artist to? ")
+file.tag.album = input("What do you want to change the album to? ")
+
+file.tag.save()
