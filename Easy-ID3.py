456b1bcb5839d4d2220eff6cb4768284a26b2649
Dean Jennings <puffin.develop@gmail.com>
2021-02-19 14:20:52 +0000

    Add edit_tags and print_tags functions, create tui menu

--- Easy-ID3.py
+++ Easy-ID3.py
@@ -1,5 +1,18 @@
 import eyed3
 
+def edit_tags(file, **kwargs):
+    
+    if 'artist' in kwargs.keys(): file.tag.artist = kwargs['artist']
+    if 'album' in kwargs.keys(): file.tag.album = kwargs['album']
+
+    print("Updated tags.")
+
+def print_tags(file):
+    print("Title: " + file.tag.title)
+    print("Artist: " + file.tag.artist)
+    print("Album: " + file.tag.album)
+
+
 
 filepath = input("Type a filepath: ")
 
@@ -9,11 +22,23 @@ except OSError:
     print("That file does not exist.")
     exit()
 
-print("Artist: " + file.tag.artist)
-print("Album: " + file.tag.album)
-print("\n")
+choice = input(\
+"""
+==============
+Eazy ID3 Menu"
+--------------
+(v)iew tags
+(e)dit tags
+==============
 
-file.tag.artist = input("What do you want to change the artist to? ")
-file.tag.album = input("What do you want to change the album to? ")
+> """)
 
-file.tag.save()
+if choice == 'v':
+    print_tags(file)
+    
+
+elif choice == 'e':
+    edit_tags(file, artist=input("Artist > "), album=input("Album > "))
+else:
+    print("That is not a valid option.")
+    exit()
